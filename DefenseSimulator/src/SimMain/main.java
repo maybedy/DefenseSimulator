b14f300072aa6f224826492c7e4c071cdade3b75
DOYUN KIM <maybedy@kaist.ac.kr>
2015-06-24 04:51:33 +0900

    d

---
+++ DefenseSimulator/src/SimMain/main.java
@@ -0,0 +1,25 @@
+package SimMain;
+
+import edu.kaist.seslab.ldef.engine.SimulationEngine;
+import edu.kaist.seslab.ldef.engine.SimulationEngine.LogLevel;
+
+public class main {
+
+	public main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		OutmostABM _outmostABM = new OutmostABM();
+	
+		_outmostABM.initializeScenario();
+		_outmostABM.createModels();
+		
+		SimulationEngine se = new SimulationEngine();
+		se.setSystemLog(LogLevel.None);
+		se.setEndtime(30000);
+		se.LoadModel(_outmostABM);
+		se.SimulationStart();
+	}
+
+}
